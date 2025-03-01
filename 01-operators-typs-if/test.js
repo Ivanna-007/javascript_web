6a6324b8d60065ae23432d5bf4bb98366d7d0e14
Ivanna <prokopovaivanna11@gmail.com>
2025-03-01 12:02:14 +0200

    lesson 1

---
+++ 01-operators-typs-if/test.js
@@ -0,0 +1,21 @@
+//коментар нa один рядок
+
+/*
+коментар
+на 
+багато
+рядків
+*/
+
+// змінні
+let myVariable =12;
+let number = 3.33;
+let text = "Тут текст";
+
+const MY_NAME = "Ivanna";
+let my1 = 10;
+let My1 = 10;
+
+myVariable = namber + myVariable
+
+
